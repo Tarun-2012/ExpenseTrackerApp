e91d61e1d3f559a2841899964c0516744c72929d
Tarun-2012 <tehlyani@sheridancollege.ca>
2025-03-19 03:09:46 -0400

    Initial commit: Added base project structure.

---
+++ ExpenseTrackerApp/ExpenseTrackerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseTrackerApp
+//
+//  Created by Default User on 3/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
